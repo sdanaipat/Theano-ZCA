5dceec6c4c1f3c3037afd9e9ecb461c9c1a4a781
Jo <sdanaipat@gmail.com>
2015-11-01 19:14:07 +0900

    add fit & transform

--- zca.py
+++ zca.py
@@ -15,14 +15,27 @@ class ZCA(object):
         sigma = T.dot(X_.T, X_) / X_.shape[0]
         self.sigma = theano.function([X_in], sigma)
 
-        zca_white = T.dot(T.dot(u, T.nlinalg.diag(1. / T.sqrt(s + eps))), u.T)
-        X_zca = T.dot(X_, zca_white.T)
+        Z = T.dot(T.dot(u, T.nlinalg.diag(1. / T.sqrt(s + eps))), u.T)
+        X_zca = T.dot(X_, Z.T)
         self.compute_zca = theano.function([X_in, u, s, eps], X_zca)
+
+        self._u = None
+        self._s = None
+
+
+    def fit(self, X):
+        cov = self.sigma(X)
+        u, s, _ = svd(cov)
+        self._u = u.astype(np.float32)
+        self._s = s.astype(np.float32)
+        del cov
+
+    def transform(self, X, eps):
+        return self.compute_zca(X, self._u, self._s, eps)
 
 
     def fit_transform(self, X, eps):
-        cov = self.sigma(X)
-        u, s, _ = svd(cov)
-        u = u.astype(np.float32)
-        s = s.astype(np.float32)
-        return self.compute_zca(X, u, s, eps)
+        self.fit(X)
+        return self.transform(X, eps)
+
+
